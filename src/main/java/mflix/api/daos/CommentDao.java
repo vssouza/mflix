1458a5556a32e33d95e20cf2bd454a9ab9b8cbb4
Vinicius Yamauchi <vs.souza@gmail.com>
2019-02-26 15:30:07 +0000

    Added comment delete feature.

--- src/main/java/mflix/api/daos/CommentDao.java
+++ src/main/java/mflix/api/daos/CommentDao.java
@@ -32,133 +32,147 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 @Component
 public class CommentDao extends AbstractMFlixDao {
 
-  public static String COMMENT_COLLECTION = "comments";
+    public static String COMMENT_COLLECTION = "comments";
 
-  private MongoCollection<Comment> commentCollection;
+    private MongoCollection<Comment> commentCollection;
 
-  private CodecRegistry pojoCodecRegistry;
+    private CodecRegistry pojoCodecRegistry;
 
-  private final Logger log;
+    private final Logger log;
 
-  @Autowired
-  public CommentDao(
-      MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
-    super(mongoClient, databaseName);
-    log = LoggerFactory.getLogger(this.getClass());
-    this.db = this.mongoClient.getDatabase(MFLIX_DATABASE);
-    this.pojoCodecRegistry =
-        fromRegistries(
-            MongoClientSettings.getDefaultCodecRegistry(),
-            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-    this.commentCollection =
-        db.getCollection(COMMENT_COLLECTION, Comment.class).withCodecRegistry(pojoCodecRegistry);
-  }
-
-  /**
-   * Returns a Comment object that matches the provided id string.
-   *
-   * @param id - comment identifier
-   * @return Comment object corresponding to the identifier value
-   */
-  public Comment getComment(String id) {
-    return commentCollection.find(new Document("_id", new ObjectId(id))).first();
-  }
-
-  /**
-   * Adds a new Comment to the collection. The equivalent instruction in the mongo shell would be:
-   *
-   * <p>db.comments.insertOne({comment})
-   *
-   * <p>
-   *
-   * @param comment - Comment object.
-   * @throw IncorrectDaoOperation if the insert fails, otherwise
-   * returns the resulting Comment object.
-   */
-  public Comment addComment(Comment comment) {
-
-    // TODO> Ticket - Update User reviews: implement the functionality that enables adding a new
-    // comment.
-    if(comment.getId() == null || comment.getId().isEmpty()) {
-      throw new IncorrectDaoOperation("Comment id cannot be set to null");
-    }
-    commentCollection.insertOne(comment);
-    // TODO> Ticket - Handling Errors: Implement a try catch block to
-    // handle a potential write exception when given a wrong commentId.
-    return comment;
-  }
-
-  /**
-   * Updates the comment text matching commentId and user email. This method would be equivalent to
-   * running the following mongo shell command:
-   *
-   * <p>db.comments.update({_id: commentId}, {$set: { "text": text, date: ISODate() }})
-   *
-   * <p>
-   *
-   * @param commentId - comment id string value.
-   * @param text - comment text to be updated.
-   * @param email - user email.
-   * @return true if successfully updates the comment text.
-   */
-  public boolean updateComment(String commentId, String text, String email) {
-
-    // TODO> Ticket - Update User reviews: implement the functionality that enables updating an
-    // user own comments
-    if(email == null || commentId == null || email.isEmpty() || commentId.isEmpty()) {
-      throw new IncorrectDaoOperation("Email and comment id cannot be null or empty. ObjectId must be valid.");
+    @Autowired
+    public CommentDao(
+            MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
+        super(mongoClient, databaseName);
+        log = LoggerFactory.getLogger(this.getClass());
+        this.db = this.mongoClient.getDatabase(MFLIX_DATABASE);
+        this.pojoCodecRegistry =
+                fromRegistries(
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        this.commentCollection =
+                db.getCollection(COMMENT_COLLECTION, Comment.class).withCodecRegistry(pojoCodecRegistry);
     }
 
-    UpdateResult result = commentCollection.updateOne(
+    /**
+     * Returns a Comment object that matches the provided id string.
+     *
+     * @param id - comment identifier
+     * @return Comment object corresponding to the identifier value
+     */
+    public Comment getComment(String id) {
+        return commentCollection.find(new Document("_id", new ObjectId(id))).first();
+    }
+
+    /**
+     * Adds a new Comment to the collection. The equivalent instruction in the mongo shell would be:
+     *
+     * <p>db.comments.insertOne({comment})
+     *
+     * <p>
+     *
+     * @param comment - Comment object.
+     * @throw IncorrectDaoOperation if the insert fails, otherwise
+     * returns the resulting Comment object.
+     */
+    public Comment addComment(Comment comment) {
+
+        // TODO> Ticket - Update User reviews: implement the functionality that enables adding a new
+        // comment.
+        if (comment.getId() == null || comment.getId().isEmpty()) {
+            throw new IncorrectDaoOperation("Comment id cannot be set to null");
+        }
+        commentCollection.insertOne(comment);
+        // TODO> Ticket - Handling Errors: Implement a try catch block to
+        // handle a potential write exception when given a wrong commentId.
+        return comment;
+    }
+
+    /**
+     * Updates the comment text matching commentId and user email. This method would be equivalent to
+     * running the following mongo shell command:
+     *
+     * <p>db.comments.update({_id: commentId}, {$set: { "text": text, date: ISODate() }})
+     *
+     * <p>
+     *
+     * @param commentId - comment id string value.
+     * @param text      - comment text to be updated.
+     * @param email     - user email.
+     * @return true if successfully updates the comment text.
+     */
+    public boolean updateComment(String commentId, String text, String email) {
+
+        // TODO> Ticket - Update User reviews: implement the functionality that enables updating an
+        // user own comments
+        if (email == null || commentId == null || email.isEmpty() || commentId.isEmpty()) {
+            throw new IncorrectDaoOperation("Email and comment id cannot be null or empty. ObjectId must be valid.");
+        }
+
+        UpdateResult result = commentCollection.updateOne(
+                Filters.and(
+                        Filters.eq("_id", new ObjectId(commentId)),
+                        Filters.eq("email", email)
+                ),
+                Updates.combine(
+                        Updates.set("text", text),
+                        Updates.set("date", new Date())
+                )
+        );
+
+        if (result.getMatchedCount() > 0) {
+            return true;
+        }
+        // TODO> Ticket - Handling Errors: Implement a try catch block to
+        // handle a potential write exception when given a wrong commentId.
+        return false;
+    }
+
+    /**
+     * Deletes comment that matches user email and commentId.
+     *
+     * @param commentId - commentId string value.
+     * @param email     - user email value.
+     * @return true if successful deletes the comment.
+     */
+    public boolean deleteComment(String commentId, String email) {
+        // TODO> Ticket Delete Comments - Implement the method that enables the deletion of a user
+        // comment
+        // TIP: make sure to match only users that own the given commentId
+        if(commentId == null || email == null || commentId.isEmpty() || email.isEmpty()) {
+            throw new IllegalArgumentException("A comment id and an email must be provided to remove the user comment.");
+        }
+
+        DeleteResult result = commentCollection.deleteOne(
             Filters.and(
-                    Filters.eq("_id", new ObjectId(commentId)),
-                    Filters.eq("email", email)
-            ),
-            Updates.combine(
-                    Updates.set("text", text),
-                    Updates.set("date", new Date())
+                Filters.eq("_id", new ObjectId(commentId)),
+                Filters.eq("email", email)
             )
-    );
+        );
 
-    if(result.getMatchedCount() > 0) {
-      return true;
+        if(result.getDeletedCount() > 0) {
+            return true;
+        }
+        // TODO> Ticket Handling Errors - Implement a try catch block to
+        // handle a potential write exception when given a wrong commentId.
+        return false;
     }
-    // TODO> Ticket - Handling Errors: Implement a try catch block to
-    // handle a potential write exception when given a wrong commentId.
-    return false;
-  }
 
-  /**
-   * Deletes comment that matches user email and commentId.
-   *
-   * @param commentId - commentId string value.
-   * @param email - user email value.
-   * @return true if successful deletes the comment.
-   */
-  public boolean deleteComment(String commentId, String email) {
-    // TODO> Ticket Delete Comments - Implement the method that enables the deletion of a user
-    // comment
-    // TIP: make sure to match only users that own the given commentId
-    // TODO> Ticket Handling Errors - Implement a try catch block to
-    // handle a potential write exception when given a wrong commentId.
-    return false;
-  }
-
-  /**
-   * Ticket: User Report - produce a list of users that comment the most in the website. Query the
-   * `comments` collection and group the users by number of comments. The list is limited to up most
-   * 20 commenter.
-   *
-   * @return List {@link Critic} objects.
-   */
-  public List<Critic> mostActiveCommenters() {
-    List<Critic> mostActive = new ArrayList<>();
-    // // TODO> Ticket: User Report - execute a command that returns the
-    // // list of 20 users, group by number of comments. Don't forget,
-    // // this report is expected to be produced with an high durability
-    // // guarantee for the returned documents. Once a commenter is in the
-    // // top 20 of users, they become a Critic, so mostActive is composed of
-    // // Critic objects.
-    return mostActive;
-  }
+    /**
+     * Ticket: User Report - produce a list of users that comment the most in the website. Query the
+     * `comments` collection and group the users by number of comments. The list is limited to up most
+     * 20 commenter.
+     *
+     * @return List {@link Critic} objects.
+     */
+    public List<Critic> mostActiveCommenters() {
+        List<Critic> mostActive = new ArrayList<>();
+        // // TODO> Ticket: User Report - execute a command that returns the
+        // // list of 20 users, group by number of comments. Don't forget,
+        // // this report is expected to be produced with an high durability
+        // // guarantee for the returned documents. Once a commenter is in the
+        // // top 20 of users, they become a Critic, so mostActive is composed of
+        // // Critic objects.
+        return mostActive;
+    }
 }
